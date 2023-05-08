f54ac73586e1dcc97246798197bf06093613269b
Wako Anindo <wa@administrate.co>
2023-05-08 17:32:55 +0100

    create module that will handle the logic for getting an access token

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -1,11 +1,34 @@
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-const redirectUri = 'http://localhost:3000/'
+const redirectUri = 'http://localhost:3000/';
 let accessToken;
 
 const Spotify = {
     getAccessToken() {
         if (accessToken) {
-            return accessToken
+            return accessToken;
         }
-    }
-}
+
+        // Check if the access token is in the URL hash
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            // Clear the access token and expiration timer when the token expires
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+
+            return accessToken;
+        } else {
+            // Redirect the user to the Spotify authorisation page if access token is not in the URL hash
+            const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+            window.location = accessUrl;
+        }
+    },
+
+
+};
+
+export default Spotify;
